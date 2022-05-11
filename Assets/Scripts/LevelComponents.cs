70266b56bf7eec52befe1c87f7348d69c3e42238
Adrian Camp <AWCamp@student.fullsail.edu>
2022-05-11 16:29:58 -0400

    more level progression, as well as scripts created for prefabs.

---
+++ Assets/Scripts/LevelComponents.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComponents : MonoBehaviour
+{
+    public bool doesPlatformMove;
+    public bool returnDirection;
+    public float speed;
+    public int direction_360;
+    public int direction2_360;//direction 2 is the direction the block goes next, then back direciton one, then 2, 1, ect. 
+    public int timeLengthMove;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
