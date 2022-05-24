b18a6684fa3de122a8f2e9fd9b72045da1f4f2a8
Adrian Camp <AWCamp@student.fullsail.edu>
2022-05-24 13:38:28 -0400

    added a moving platform. forgot to switching to LevelComponents branch.

--- Assets/Scripts/LevelComponents.cs
+++ Assets/Scripts/LevelComponents.cs
@@ -9,7 +9,7 @@ public class LevelComponents : MonoBehaviour
     public bool doesPlatformMove;
     public bool doesPlatformDisappear;
     public bool returnDirection;
-    public float speed;
+    //public float speed;
     [Range(0, 360)]
     public int direction_360;
     [Range(0, 360)]
@@ -26,184 +26,225 @@ public class LevelComponents : MonoBehaviour
     private bool direction1Or2 = false;
     public Rigidbody2D rb;
 
+    //code from class
+    [SerializeField] Transform destinationPosition;
+    [SerializeField] int speed;
+
+    Vector2 startingPosition;
+    Vector2 target;
+
+
+
+    
+
             void Start()
     {
+        startingPosition = transform.position;
+        target = destinationPosition.position;
         rb = GetComponent<Rigidbody2D>();
         currentPosition = transform.position; //Setting CurrentPosition to adjust and track.
         timeLengthTracker = timeLengthMove; //setting a tracker to count down the move time.
         ChangeTotalMove();
     }
 
-    void ChangeTotalMove()
+    void MovePlatform()
     {
-            if(direction1Or2 == false)//false is direction2
-            {
-                      
-                if (direction2_360 > 0 && direction2_360< 90)
-                {
-                    radians = (float) (direction2_360* Math.PI / 180);
-                    result = (float) Math.Tan(radians);
-                    directionAngleQuadrant = 1;
-                    moveX = 1 / (result + 1);
-                    moveY = result / (result + 1);
-                }
-                else if (direction2_360 > 90 && direction2_360 < 180)
-                {
-                 radians = (float)((180 - direction2_360) * Math.PI / 180);
-                 result = (float)Math.Tan(radians);
-                 directionAngleQuadrant = 2;
-                 moveX = -1 * (1 / (result + 1));
-                 moveY = result / (result + 1);
-                }
-                else if (direction2_360 > 180 && direction2_360 < 270)
-                {
-                radians = (float)((direction2_360 - 180) * Math.PI / 180);
-                result = (float)Math.Tan(radians);
-                directionAngleQuadrant = 3;
-                moveX = -1 * (1 / (result + 1));
-                 moveY = -1 * (result / (result + 1));
-                }
-                else if (direction2_360 > 270 && direction2_360 < 360)
-{
-    directionAngleQuadrant = 4;
-    radians = (float)((360 - direction2_360) * Math.PI / 180);
-    result = (float)Math.Tan(radians);
-    moveX = 1 / (result + 1);
-    moveY = -1 * (result / (result + 1));
-}
-                else if (direction2_360 == 90)
-{
-    directionAngleQuadrant = 5; //Moving Directly Up on the Y Axis ("Quadrant" 5)
-    moveX = 0;
-    moveY = 1;
-}
-                else if (direction2_360 == 180)
-{
-    directionAngleQuadrant = 6; //Moving directly Left on the X Axis ("Quadrant" 6)
-    moveX = -1;
-    moveY = 0;
-}
-                else if (direction2_360 == 270)
-{
-    directionAngleQuadrant = 7; // Moving Directly Down on the Y Axis ("Quadrant" 7)
-    moveX = 0;
-    moveY = 1;
-}
-                else if (direction2_360 == 360 || direction2_360 == 0)
-{
-    directionAngleQuadrant = 8; //Moving directly Right on the X Axis("Quadrant" 8)
-    moveX = 1;
-    moveY = 0;
-}
 
-                totalMove = new Vector3(moveX, moveY, 0f);
-                direction1Or2 = true; 
-            }
 
-            else if ( direction1Or2 == true)
-            {
-                if (direction_360 > 0 && direction_360 < 90)
-            {
-                radians = (float)(direction_360 * Math.PI / 180);
-                result = (float)Math.Tan(radians);
-                directionAngleQuadrant = 1;
-                moveX = 1 / (result + 1);
-                moveY = result / (result + 1);
-            }
-                else if (direction_360 > 90 && direction_360 < 180)
-            {
-                radians = (float)((180 - direction_360) * Math.PI / 180);
-                result = (float)Math.Tan(radians);
-                directionAngleQuadrant = 2;
-                moveX = -1 * (1 / (result + 1));
-                moveY = result / (result + 1);
-            }
-                else if (direction_360 > 180 && direction_360 < 270)
-            {
-                radians = (float)((direction_360 - 180) * Math.PI / 180);
-                result = (float)Math.Tan(radians);
-                directionAngleQuadrant = 3;
-                moveX = -1 * (1 / (result + 1));
-                moveY = -1 * (result / (result + 1));
-            }
-                else if (direction_360 > 270 && direction_360 < 360)
-            {
-                directionAngleQuadrant = 4;
-                radians = (float)((360 - direction_360) * Math.PI / 180);
-                result = (float)Math.Tan(radians);
-
-                moveX = 1 / (result + 1);
-                moveY = -1 * (result / (result + 1));
-            }
-                else if (direction_360 == 90)
-            {
-                directionAngleQuadrant = 5; //Moving Directly Up on the Y Axis ("Quadrant" 5)
-                moveX = 0;
-                moveY = 1;
-            }
-                else if (direction_360 == 180)
-            {
-                directionAngleQuadrant = 6; //Moving directly Left on the X Axis ("Quadrant" 6)
-                moveX = -1;
-                moveY = 0;
-            }
-                else if (direction_360 == 270)
-            {
-                directionAngleQuadrant = 7; // Moving Directly Down on the Y Axis ("Quadrant" 7)
-                moveX = 0;
-                moveY = 1;
-            }
-                else if (direction_360 == 360 || direction_360 == 0)
-            {
-                directionAngleQuadrant = 8; //Moving directly Right on the X Axis("Quadrant" 8)
-                moveX = 1;
-                moveY = 0;
-            }
-                totalMove = new Vector3(moveX, moveY, 0f);
-                direction1Or2 = false;
-            }
-            
-     }//sets direction.
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        currentPosition = transform.position;
-        if (doesPlatformMove == true)
-        {
-            if (timeLengthTracker > 0)
-            {
-                
-                rb.velocity = totalMove * speed;
-                timeLengthTracker--;
-            }
-            else if (nextMoveTime < Time.deltaTime)
-            {
-                Vector3 vel = rb.velocity;
-                rb.velocity = vel.normalized * 0f;
-                nextMoveTime = Time.deltaTime + pauseBetweenDirectionSwitch;
-            }
-            else if (timeLengthTracker <= 0 && returnDirection == true)
-            {
-
-                Vector3 vel = rb.velocity;
-                rb.velocity = vel.normalized * 0f;
-                ChangeTotalMove();
-                timeLengthTracker = timeLengthMove;
-            }
-        }
     }
+
+//    void ChangeTotalMove()
+//    {
+//            if(direction1Or2 == false)//false is direction2
+//            {
+                      
+//                if (direction2_360 > 0 && direction2_360< 90)
+//                {
+//                    radians = (float) (direction2_360* Math.PI / 180);
+//                    result = (float) Math.Tan(radians);
+//                    directionAngleQuadrant = 1;
+//                    moveX = 1 / (result + 1);
+//                    moveY = result / (result + 1);
+//                }
+//                else if (direction2_360 > 90 && direction2_360 < 180)
+//                {
+//                 radians = (float)((180 - direction2_360) * Math.PI / 180);
+//                 result = (float)Math.Tan(radians);
+//                 directionAngleQuadrant = 2;
+//                 moveX = -1 * (1 / (result + 1));
+//                 moveY = result / (result + 1);
+//                }
+//                else if (direction2_360 > 180 && direction2_360 < 270)
+//                {
+//                radians = (float)((direction2_360 - 180) * Math.PI / 180);
+//                result = (float)Math.Tan(radians);
+//                directionAngleQuadrant = 3;
+//                moveX = -1 * (1 / (result + 1));
+//                 moveY = -1 * (result / (result + 1));
+//                }
+//                else if (direction2_360 > 270 && direction2_360 < 360)
+//{
+//    directionAngleQuadrant = 4;
+//    radians = (float)((360 - direction2_360) * Math.PI / 180);
+//    result = (float)Math.Tan(radians);
+//    moveX = 1 / (result + 1);
+//    moveY = -1 * (result / (result + 1));
+//}
+//                else if (direction2_360 == 90)
+//{
+//    directionAngleQuadrant = 5; //Moving Directly Up on the Y Axis ("Quadrant" 5)
+//    moveX = 0;
+//    moveY = 1;
+//}
+//                else if (direction2_360 == 180)
+//{
+//    directionAngleQuadrant = 6; //Moving directly Left on the X Axis ("Quadrant" 6)
+//    moveX = -1;
+//    moveY = 0;
+//}
+//                else if (direction2_360 == 270)
+//{
+//    directionAngleQuadrant = 7; // Moving Directly Down on the Y Axis ("Quadrant" 7)
+//    moveX = 0;
+//    moveY = 1;
+//}
+//                else if (direction2_360 == 360 || direction2_360 == 0)
+//{
+//    directionAngleQuadrant = 8; //Moving directly Right on the X Axis("Quadrant" 8)
+//    moveX = 1;
+//    moveY = 0;
+//}
+
+//                totalMove = new Vector3(moveX, moveY, 0f);
+//                direction1Or2 = true; 
+//            }
+
+//            else if ( direction1Or2 == true)
+//            {
+//                if (direction_360 > 0 && direction_360 < 90)
+//            {
+//                radians = (float)(direction_360 * Math.PI / 180);
+//                result = (float)Math.Tan(radians);
+//                directionAngleQuadrant = 1;
+//                moveX = 1 / (result + 1);
+//                moveY = result / (result + 1);
+//            }
+//                else if (direction_360 > 90 && direction_360 < 180)
+//            {
+//                radians = (float)((180 - direction_360) * Math.PI / 180);
+//                result = (float)Math.Tan(radians);
+//                directionAngleQuadrant = 2;
+//                moveX = -1 * (1 / (result + 1));
+//                moveY = result / (result + 1);
+//            }
+//                else if (direction_360 > 180 && direction_360 < 270)
+//            {
+//                radians = (float)((direction_360 - 180) * Math.PI / 180);
+//                result = (float)Math.Tan(radians);
+//                directionAngleQuadrant = 3;
+//                moveX = -1 * (1 / (result + 1));
+//                moveY = -1 * (result / (result + 1));
+//            }
+//                else if (direction_360 > 270 && direction_360 < 360)
+//            {
+//                directionAngleQuadrant = 4;
+//                radians = (float)((360 - direction_360) * Math.PI / 180);
+//                result = (float)Math.Tan(radians);
+
+//                moveX = 1 / (result + 1);
+//                moveY = -1 * (result / (result + 1));
+//            }
+//                else if (direction_360 == 90)
+//            {
+//                directionAngleQuadrant = 5; //Moving Directly Up on the Y Axis ("Quadrant" 5)
+//                moveX = 0;
+//                moveY = 1;
+//            }
+//                else if (direction_360 == 180)
+//            {
+//                directionAngleQuadrant = 6; //Moving directly Left on the X Axis ("Quadrant" 6)
+//                moveX = -1;
+//                moveY = 0;
+//            }
+//                else if (direction_360 == 270)
+//            {
+//                directionAngleQuadrant = 7; // Moving Directly Down on the Y Axis ("Quadrant" 7)
+//                moveX = 0;
+//                moveY = 1;
+//            }
+//                else if (direction_360 == 360 || direction_360 == 0)
+//            {
+//                directionAngleQuadrant = 8; //Moving directly Right on the X Axis("Quadrant" 8)
+//                moveX = 1;
+//                moveY = 0;
+//            }
+//                totalMove = new Vector3(moveX, moveY, 0f);
+//                direction1Or2 = false;
+//            }
+            
+//     }//sets direction.
+    // Update is called once per frame
+
+    //void FixedUpdate()
+    //{
+    //    currentPosition = transform.position;
+    //    if (doesPlatformMove == true)
+    //    {
+    //        if (timeLengthTracker > 0)
+    //        {
+                
+    //            rb.velocity = totalMove * speed;
+    //            timeLengthTracker--;
+    //        }
+    //        else if (nextMoveTime < Time.deltaTime)
+    //        {
+    //            Vector3 vel = rb.velocity;
+    //            rb.velocity = vel.normalized * 0f;
+    //            nextMoveTime = Time.deltaTime + pauseBetweenDirectionSwitch;
+    //        }
+    //        else if (timeLengthTracker <= 0 && returnDirection == true)
+    //        {
+
+    //            Vector3 vel = rb.velocity;
+    //            rb.velocity = vel.normalized * 0f;
+    //            ChangeTotalMove();
+    //            timeLengthTracker = timeLengthMove;
+    //        }
+    //    }
+    //}
+
 
 
 
 
 
     //Shows draws out for direction.
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if(transform.position == target)
+        {
+           if( target == startingPosition)
+            {
+                target = destinationPosition;
+            }
+           else if(target == destinationPosition)
+            {
+                target = startingPosition;
+            }
+        }
+        
+
+    }
+
+
     private void OnDrawGizmosSelected()
     {
         if (showMovePath == true)
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawLine(this.transform.position, totalMove * 3);
+            //Gizmos.DrawLine(this.transform.position, totalMove * 3);
         }
     //    Gizmos.DrawWireSphere();
     }
